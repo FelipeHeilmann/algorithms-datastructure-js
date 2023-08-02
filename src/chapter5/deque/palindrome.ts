502862ed0bd8b1c70f9e06f192016d5e02517210
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-08-02 19:07:14 -0300

    feat: add check palindrome with deque

---
+++ src/chapter5/deque/palindrome.ts
@@ -0,0 +1,27 @@
+import { Deque } from "./deque"
+
+function checkPalindrome(string: string){
+    if(string === undefined || string === null || string != null && string.length === 0){
+        return false
+    }
+
+    const deque = new Deque()
+    const lowerString = string.toLocaleLowerCase().split(' ').join(' ')
+    let isEqual = true
+    let firstChar, lastChar
+    for(let i = 0; i < lowerString.length; i++){
+        deque.addBack(lowerString.charAt(i))
+    }
+
+    while(deque.size() > 1 && isEqual){
+        firstChar = deque.removeFront()
+        lastChar = deque.removeBack()
+
+        if(firstChar !== lastChar)
+            isEqual = false
+    }
+
+    return isEqual
+}
+
+console.log(checkPalindrome('ovo'))

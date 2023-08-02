9b3e57c010b097a73443e19e54995351096765b5
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-08-02 15:16:14 -0300

    feat: converting decimal base to binary base

--- src/chapter4/index.ts
+++ src/chapter4/index.ts
@@ -27,3 +27,22 @@ stack.pop()
 
 console.log(stack2.toString())
 
+function decimalToBinary(decNumer: number){
+    const remStack = new Stack()
+    let number = decNumer
+    let rem
+    let binaryString = ''
+    while(number > 0){
+        rem = Math.floor(number % 2)
+        remStack.push(rem)
+        number = Math.floor(number /2)
+    }
+
+    while(!remStack.isEmpty()){
+        binaryString += `${remStack.pop()}`
+    }
+    return binaryString
+}
+
+
+

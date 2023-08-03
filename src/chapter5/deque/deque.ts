cae4ff0e4856cfd878563444706ab1035ef804a5
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-08-03 15:31:06 -0300

    docs: comments to explain queue and deque implementation

--- src/chapter5/deque/deque.ts
+++ src/chapter5/deque/deque.ts
@@ -1,83 +1,106 @@
-export class Deque<T>{
-    private items: { [key: number]: T }
-    private count: number
-    private lowestCount: number
+export class Deque<T> {
+    private items: { [key: number]: T };
+    private count: number;
+    private lowestCount: number;
 
-    constructor(){
-        this.items = {}
-        this.count = 0
-        this.lowestCount = 0
+    constructor() {
+        this.items = {};
+        this.count = 0;
+        this.lowestCount = 0;
     }
-    addFront(element: T){
-        if(this.isEmpty()){
-            this.addBack(element)
-        }
-        else if(this.lowestCount > 0){
-            this.lowestCount--
-            this.items[this.lowestCount] =  element
-        }
-        else{
-            for(let i = this.count; i > 0; i--){
-                this.items[i] = this.items[i - 1]
+
+    // Add an element to the front of the deque
+    addFront(element: T) {
+        if (this.isEmpty()) {
+            // If the deque is empty, add the element to the back (alternative implementation).
+            this.addBack(element);
+        } else if (this.lowestCount > 0) {
+            // If the deque is not empty, but there is space at the back (lowestCount > 0),
+            // move the elements to the correct position and insert the new element at the front.
+            this.lowestCount--;
+            this.items[this.lowestCount] = element;
+        } else {
+            // If the deque is not empty and there is no space at the back,
+            // move all elements one position forward and insert the new element at the front.
+            for (let i = this.count; i > 0; i--) {
+                this.items[i] = this.items[i - 1];
             }
-            this.count++
-            this.lowestCount = 0
-            this.items[0] = element
+            this.count++;
+            this.lowestCount = 0;
+            this.items[0] = element;
         }
     }
-    addBack(element: T){
-        this.items[this.count] = element
-        this.count++
-    }
-    removeFront(){
-        if(this.isEmpty())
-            return undefined
-        
-        const result = this.items[this.lowestCount]
-        delete this.items[this.lowestCount]
-        this.lowestCount++
 
-        return result
+    // Add an element to the back of the deque
+    addBack(element: T) {
+        this.items[this.count] = element;
+        this.count++;
     }
-    removeBack(){
-        if(this.isEmpty())
-        return undefined
-    
-        const result = this.items[this.count - 1]
-        delete this.items[this.count - 1]
-        this.count--
 
-        return result
+    // Remove and return the front element from the deque
+    removeFront() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const result = this.items[this.lowestCount];
+        delete this.items[this.lowestCount];
+        this.lowestCount++;
+        return result;
     }
-    peekFront(){
-        if(this.isEmpty())
-            return undefined
-        return this.items[this.lowestCount]
+
+    // Remove and return the back element from the deque
+    removeBack() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const result = this.items[this.count - 1];
+        delete this.items[this.count - 1];
+        this.count--;
+        return result;
     }
-    peekBack(){
-        if(this.isEmpty())
-            return undefined
-        return this.items[this.count - 1] 
+
+    // Get the front element of the deque without removing it
+    peekFront() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.lowestCount];
     }
-    isEmpty(){
-        return this.count - this.lowestCount === 0
+
+    // Get the back element of the deque without removing it
+    peekBack() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.count - 1];
     }
-    size(){
-        return this.count - this.lowestCount
+
+    // Check if the deque is empty
+    isEmpty() {
+        return this.count - this.lowestCount === 0;
     }
-    clear(){
-        this.items = {}
-        this.count = 0
-        this.lowestCount = 0
+
+    // Get the current size of the deque
+    size() {
+        return this.count - this.lowestCount;
     }
+
+    // Clear the deque, removing all elements
+    clear() {
+        this.items = {};
+        this.count = 0;
+        this.lowestCount = 0;
+    }
+
+    // String representation of the deque
     toString() {
         if (this.isEmpty()) {
             return '';
         }
-        let string = `${this.items[this.lowestCount]}`
+        let string = `${this.items[this.lowestCount]}`;
         for (let index = this.lowestCount + 1; index < this.count; index++) {
-            string = `${string},${this.items[index]}`
+            string = `${string},${this.items[index]}`;
         }
-        return string
+        return string;
     }
 }
--- src/chapter5/queue/queue.ts
+++ src/chapter5/queue/queue.ts
@@ -1,50 +1,65 @@
-export class Queue<T>{
-    private items: { [key: number]: T }
-    private count: number
-    private lowestCount: number
+export class Queue<T> {
+    private items: { [key: number]: T };
+    private count: number;
+    private lowestCount: number;
 
-    constructor(){
-        this.items = {}
-        this.count = 0
-        this.lowestCount = 0
+    constructor() {
+        this.items = {};
+        this.count = 0;
+        this.lowestCount = 0;
     }
-    enqueue(element: T){
-        this.items[this.count] = element
-        this.count++
-    }
-    dequeue(){
-        if(this.isEmpty())
-            return undefined
-        const result = this.items[this.lowestCount]
-        
-        delete this.items[this.lowestCount]
-        this.lowestCount++
 
-        return result
+    // Add an element to the end of the queue
+    enqueue(element: T) {
+        this.items[this.count] = element;
+        this.count++;
     }
-    peek(){
-        if(this.isEmpty())
-            return undefined
-        return this.items[this.lowestCount]
-    }
-    isEmpty(){
-        return this.count - this.lowestCount === 0
-    }
-    size(){
-        return this.count - this.lowestCount
-    }
-    clear(){
-        this.items = {}
-        this.count = 0
-        this.lowestCount = 0
-    }
-    toString(){
-        if(this.isEmpty())
-            return ''
-        let objString: string = `${this.items[this.lowestCount]}`
-        for(let i = this.lowestCount + 1; i < this.count; i++){
-            objString = `${objString}, ${this.items[i]}`
+
+    // Remove and return the front element from the queue
+    dequeue() {
+        if (this.isEmpty()) {
+            return undefined;
         }
-        return objString
+        const result = this.items[this.lowestCount];
+        delete this.items[this.lowestCount];
+        this.lowestCount++;
+        return result;
+    }
+
+    // Get the front element of the queue without removing it
+    peek() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.lowestCount];
+    }
+
+    // Check if the queue is empty
+    isEmpty() {
+        return this.count - this.lowestCount === 0;
+    }
+
+    // Get the current size of the queue
+    size() {
+        return this.count - this.lowestCount;
+    }
+
+    // Clear the queue, removing all elements
+    clear() {
+        this.items = {};
+        this.count = 0;
+        this.lowestCount = 0;
+    }
+
+    // String representation of the queue
+    toString() {
+        if (this.isEmpty()) {
+            return '';
+        }
+        let objString: string = `${this.items[this.lowestCount]}`;
+        for (let i = this.lowestCount + 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`;
+        }
+        return objString;
     }
 }

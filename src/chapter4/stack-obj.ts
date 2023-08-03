2cb49ecfe3fe75180f490df90d22c0a1520b32a4
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-08-03 15:26:09 -0300

    docs: comments to explain stack implementation

--- src/chapter4/stack-obj.ts
+++ src/chapter4/stack-obj.ts
@@ -1,51 +1,67 @@
-/*Implementing a stack using javascript object*/
-
+// Implementing a stack using a JavaScript object
 export class StackObj<T> {
-    private count: number
-    private items: { [key: number]: T }
+    // Private members of the class
+    private count: number; // Keeps track of the number of elements in the stack
+    private items: { [key: number]: T }; // Object to store the elements of the stack
 
+    // Constructor to initialize the stack
     constructor() {
-        this.count = 0
-        this.items = {}
+        this.count = 0; // Initialize the count to zero, as the stack is empty initially
+        this.items = {}; // Initialize an empty object to store the stack elements
     }
 
+    // Method to add an element to the top of the stack
     push(element: T) {
-        this.items[this.count] = element
-        this.count++
+        this.items[this.count] = element; // Add the element to the 'items' object with the 'count' as the key
+        this.count++; // Increment the count to reflect the addition of a new element
     }
-    pop(){
-        if(this.isEmpty())
-            return undefined
+
+    // Method to remove and return the element from the top of the stack
+    pop() {
+        if (this.isEmpty()) // Check if the stack is empty
+            return undefined; // If empty, return 'undefined'
+
+        this.count--; // Decrement the count to point to the last element in the stack
+        const item = this.items[this.count]; // Get the last element from the 'items' object
+        delete this.items[this.count]; // Delete the last element from the 'items' object
+
+        return item; // Return the removed element
+    }
+
+    // Method to return the element at the top of the stack without removing it
+    peek() {
+        if (this.isEmpty()) // Check if the stack is empty
+            return undefined; // If empty, return 'undefined'
         
-        this.count--
-        const item = this.items[this.count]
-        delete this.items[this.count]
+        // Return the last element in the 'items' object without removing it
+        return this.items[this.count - 1];
+    }
 
-        return item
+    // Method to check if the stack is empty
+    isEmpty() {
+        return this.count === 0; // Returns 'true' if the count is zero (empty), otherwise 'false'
+    }
 
+    // Method to return the number of elements in the stack
+    size() {
+        return this.count; // Returns the value of 'count', representing the number of elements
     }
-    peek(){
-        if(this.isEmpty())
-            return undefined
-        return this.items[this.count - 1]    
+
+    // Method to clear the stack by resetting the 'items' object and 'count' to zero
+    clear() {
+        this.items = {}; // Reset the 'items' object to an empty object
+        this.count = 0; // Reset the 'count' to zero, making the stack empty
     }
-    isEmpty(){
-        return this.count === 0
-    }
-    size(){
-        return this.count
-    }
-    clear(){
-        this.items = {}
-        this.count = 0
-    }
-    toString(){
-        if(this.isEmpty())
-            return ''
-        let objString: string = `${this.items[0]}`
-        for(let i = 1; i < this.count; i++){
-            objString = `${objString}, ${this.items[i]}`
+
+    // Method to convert the stack elements to a comma-separated string
+    toString() {
+        if (this.isEmpty()) // Check if the stack is empty
+            return ''; // If empty, return an empty string
+        
+        let objString: string = `${this.items[0]}`; // Start with the first element in 'items'
+        for (let i = 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`; // Append each element with a comma separator
         }
-        return objString    
+        return objString; // Return the comma-separated string representation of the stack
     }
 }
--- src/chapter4/stack.ts
+++ src/chapter4/stack.ts
@@ -1,30 +1,42 @@
 /*
-A LIFO (Last in First out) datastructure
-as we just need to access the element on the top of the array, all the methods are O(1)
+A LIFO (Last in First out) data structure.
+As we just need to access the element on the top of the array, all the methods are O(1).
 */
 
-export class Stack<T>{
-    private items: T[] = []
-    constructor(){
-        this.items = []
+export class Stack<T> {
+    private items: T[] = [];
+
+    constructor() {
+        this.items = [];
     }
 
-    push(element: T){
-        this.items.push(element)
+    // Method to add an element to the top of the stack
+    push(element: T) {
+        this.items.push(element);
     }
-    pop(){
-        return this.items.pop()
+
+    // Method to remove and return the element from the top of the stack
+    pop() {
+        return this.items.pop();
     }
-    peek(){
-        return this.items[this.items.length - 1]
+
+    // Method to return the element at the top of the stack without removing it
+    peek() {
+        return this.items[this.items.length - 1];
     }
-    isEmpty(){
-        return this.items.length === 0
+
+    // Method to check if the stack is empty
+    isEmpty() {
+        return this.items.length === 0;
     }
-    size(){
-        return this.items.length
+
+    // Method to return the number of elements in the stack
+    size() {
+        return this.items.length;
     }
-    clear(){
-        this.items = []
+
+    // Method to clear the stack by resetting the 'items' array to an empty array
+    clear() {
+        this.items = [];
     }
 }

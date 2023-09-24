819511019eccc0e00a18a69c68d6f356be185e0f
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-09-24 17:47:17 -0300

    chore: import inf from utils

--- src/chapter13/dijkstra.ts
+++ src/chapter13/dijkstra.ts
@@ -1,3 +1,28 @@
+/*
+Dijkstra's algorithm is a graph traversal and shortest path-finding algorithm that operates by iteratively selecting the vertex with the shortest known distance from the source vertex and relaxing (updating) the distances to its neighboring vertices. It is widely used for finding the shortest path between a source vertex and all other vertices in a weighted graph.
+
+Here's how Dijkstra's algorithm works:
+
+Initialize a list of distances for each vertex, setting the distance to the source vertex as 0 and all other distances as infinity.
+Create a priority queue (min-heap) to keep track of vertices with the smallest tentative distance.
+Start with the source vertex and add it to the priority queue with a distance of 0.
+While the priority queue is not empty:
+Extract the vertex with the smallest distance from the priority queue (let's call it the current vertex).
+For each neighboring vertex that is not yet visited:
+Calculate the tentative distance from the source to this neighboring vertex through the current vertex.
+If this tentative distance is shorter than the current known distance to the neighboring vertex, update the distance.
+Add the neighboring vertex to the priority queue with its updated distance.
+Repeat step 4 until all vertices have been processed.
+The key characteristics of Dijkstra's algorithm are:
+
+It guarantees that the shortest path from the source vertex to all other vertices is found.
+It relies on a priority queue (min-heap) to efficiently select the vertex with the smallest distance.
+It is used in various applications, including network routing, navigation systems, and critical path analysis in project management.
+Dijkstra's algorithm is a fundamental tool for solving shortest path problems in graphs with weighted edges, ensuring that the shortest path distances are accurately computed.
+*/
+
+
+
 var graph = [
     [0, 2, 4, 0, 0, 0],
     [0, 0, 1, 4, 2, 0],
@@ -7,7 +32,7 @@ var graph = [
     [0, 0, 0, 0, 0, 0]
 ]
 
-const INF = Number.MAX_SAFE_INTEGER
+export const INF = Number.MAX_SAFE_INTEGER
 const dijkstra = (graph: number[][], src: number) => {
     const dist = []
     const visited = []

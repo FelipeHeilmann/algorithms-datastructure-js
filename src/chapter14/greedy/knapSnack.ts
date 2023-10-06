680a99546af7ac5a948ff28afda05ca7e2d02740
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-10-06 09:10:22 -0300

    feat: add knap snack greedy problem

---
+++ src/chapter14/greedy/knapSnack.ts
@@ -0,0 +1,17 @@
+export function knapSack(capacity: number, weights: number[], values: number[]): number {
+    const n = values.length
+    let load = 0
+    let val = 0
+  
+    for (let i = 0; i < n && load < capacity; i++) {
+      if (weights[i] <= capacity - load) {
+        val += values[i]
+        load += weights[i]
+      } else {
+        const r = (capacity - load) / weights[i];
+        val += r * values[i]
+        load += weights[i]
+      }
+    }
+    return val
+  }
